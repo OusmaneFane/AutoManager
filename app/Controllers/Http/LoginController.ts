54779e4aa95ba7b5a583ff1df64bba31a1b3d885
OusmaneFane <ousmanefane08@gmail.com>
2024-01-07 19:49:18 +0000

    change redirection

--- app/Controllers/Http/LoginController.ts
+++ app/Controllers/Http/LoginController.ts
@@ -1,94 +1,85 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import User from 'App/Models/User'
-import Hash from '@ioc:Adonis/Core/Hash'
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import User from "App/Models/User";
+import Hash from "@ioc:Adonis/Core/Hash";
 
 export default class LoginController {
+  public async index({ view }: HttpContextContract) {
+    return view.render("login.index");
+  }
 
-    public async index({ view }: HttpContextContract) {
-        return view.render('login.index')
-      }
+  public async check({
+    request,
+    response,
+    session,
+    auth,
+  }: HttpContextContract) {
+    try {
+      // verifier si les donnees sont correctes (email et mot de passe)
+      const { email, password } = request.all();
+      //console.log(email, password);
 
-      public async check({ request, response, session, auth }: HttpContextContract) {
-        try {
+      await auth.use("web").attempt(email, password);
 
+      // verifier si email existe dans la base de donnees
+      const user = await User.findBy("email", email);
+      console.log("user", await Hash.make("admin"));
 
-        // verifier si les donnees sont correctes (email et mot de passe)
-        const { email, password } = request.all();
-         console.log(email, password);
+      if (user) {
+        // verifier si le mot de passe correspond
+        const isSame = await Hash.verify(user.password, password);
+        console.log("isame", isSame);
 
-        await auth.use('web').attempt(email, password)
-
-         // verifier si email existe dans la base de donnees
-         const user = await User.findBy('email', email)
-         console.log('user', await Hash.make('admin'));
-
-         if (user) {
-
-           // verifier si le mot de passe correspond
-           const isSame = await Hash.verify(user.password, password)
-           console.log('isame', isSame);
-
-           if (isSame) {
-             // connecter l'utilisateur
-             await auth.login(user)
-             // rediriger vers la page d'accueil
-             return response.redirect().toRoute('dashboard')
-           }
-         } else {
-           session.flash('error', 'Email ou mot de passe incorrect')
-           return response.redirect().back()
-         }
-        } catch (error) {
-          console.log(error);
-
+        if (isSame) {
+          // connecter l'utilisateur
+          await auth.login(user);
+          // rediriger vers la page d'accueil
+          return response.redirect().toRoute("dashboard");
         }
-         // commande pour installer le package de authentification
-         // adonis install @adonisjs/auth
-
-       }
-       public async showRegisterForm({ view }) {
-        return view.render('register.index')
+      } else {
+        session.flash({ incorrect: "Email ou mot de passe incorrect" });
+        return response.redirect().back();
       }
-      public async register({ request, response, session }) {
-        const userData = request.only(['name', 'email']);
-        const { password, confirm_password } = request.all();
-        console.log(userData);
-        console.log(password, confirm_password);
-        
-        
-        try {
-          
-          if (password !== confirm_password) {
-            response.flash({ error: 'Les mots de passe ne correspondent pas' });
-            return response.redirect().back(); // Redirige vers le formulaire d'inscription en cas d'erreur
-          }else{
-            
-            // Hachez le mot de passe
-            const hashedPassword = await Hash.make(password);
+    } catch (error) {
+      console.log("yoooo");
+      session.flash({ incorrect: "Email ou mot de passe incorrect" });
+      return response.redirect().back();
+    }
+  }
+  public async showRegisterForm({ view }) {
+    return view.render("register.index");
+  }
+  public async register({ request, response, session }) {
+    const userData = request.only(["name", "email"]);
+    const { password, confirm_password } = request.all();
+    console.log(userData);
+    console.log(password, confirm_password);
 
-            // Créez un nouvel utilisateur avec le mot de passe haché
-            const user = new User();
-            user.name = userData.name;
-            user.email = userData.email;
-            user.password = hashedPassword;
+    try {
+      if (password !== confirm_password) {
+        response.flash({ error: "Les mots de passe ne correspondent pas" });
+        return response.redirect().back(); // Redirige vers le formulaire d'inscription en cas d'erreur
+      } else {
+        // Hachez le mot de passe
+        const hashedPassword = await Hash.make(password);
 
-            await user.save();
-            session.flash({register: 'compte créer avec succès'})
-            return response.redirect().toRoute('login'); // Redirige vers la page d'accueil après l'inscription
+        // Créez un nouvel utilisateur avec le mot de passe haché
+        const user = new User();
+        user.name = userData.name;
+        user.email = userData.email;
+        user.password = hashedPassword;
 
-          }
-
-
-        } catch (error) {
-          // Gérer les erreurs d'inscription, par exemple, si l'email est déjà pris
-          session.flash({ error: 'Erreur lors de l\'inscription' });
-          return response.redirect().back(); // Redirige vers le formulaire d'inscription en cas d'erreur
-        }
-
+        await user.save();
+        session.flash({ register: "compte créer avec succès" });
+        return response.redirect().toRoute("login"); // Redirige vers la page d'accueil après l'inscription
       }
-       public async logout({ auth, response }: HttpContextContract) {
-        await auth.logout()
-        return response.redirect().toRoute('login')
-      }
-
+    } catch (error) {
+      // Gérer les erreurs d'inscription, par exemple, si l'email est déjà pris
+      session.flash({ error: "Erreur lors de l'inscription" });
+      return response.redirect().back(); // Redirige vers le formulaire d'inscription en cas d'erreur
+    }
+  }
+  public async logout({ auth, response }: HttpContextContract) {
+    await auth.logout();
+    return response.redirect().toRoute("login");
+  }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,93 +1,107 @@
+import Route from "@ioc:Adonis/Core/Route";
 
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Application from "@ioc:Adonis/Core/Application";
 
-import Route from '@ioc:Adonis/Core/Route'
+Route.get("/", "LoginController.index").as("login");
+Route.post("/check", "LoginController.check").as("check");
+Route.get("/logout", "LoginController.logout").as("logout");
 
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Application from '@ioc:Adonis/Core/Application'
+Route.get("/dashboard", "DashboardController.index").as("dashboard");
 
-Route.get('/', 'LoginController.index').as('login');
-Route.post('/login', 'LoginController.check').as('check');
-Route.get('/logout', 'LoginController.logout').as('logout')
+Route.get("/add-vehicule", "VehiculesController.create").as("veh.create");
+Route.post("/store-vehicule", "VehiculesController.store").as("store_veh");
 
-Route.get('/dashboard', 'DashboardController.index').as('dashboard');
-
-Route.get('/add-vehicule', 'VehiculesController.create').as('veh.create');
-Route.post('/store-vehicule', 'VehiculesController.store').as('store_veh');
-
-Route.get('/modifier-vehicule', 'VehiculesController.edit').as('veh.edit');
-Route.get('/edit-vehicle/:id', 'VehiculesController.editer').as('editVehicle');
-Route.post('/update-vehicle/:id', 'VehiculesController.update').as('updateVehicle');
-Route.get('/delete-vehicle/:id', 'VehiculesController.destroy').as('deleteVehicle');
-
+Route.get("/modifier-vehicule", "VehiculesController.edit").as("veh.edit");
+Route.get("/edit-vehicle/:id", "VehiculesController.editer").as("editVehicle");
+Route.post("/update-vehicle/:id", "VehiculesController.update").as(
+  "updateVehicle"
+);
+Route.get("/delete-vehicle/:id", "VehiculesController.destroy").as(
+  "deleteVehicle"
+);
 
 // Route.get('/add-conducteur', 'ConducteursController.index').as('cond.create');
 // Route.get('/edit-conducteur', 'ConducteursController.edit').as('cond.edit');
 //
 // Route pour afficher le formulaire de création d'un conducteur
-Route.get('/create-driver', 'ConducteursController.create').as('createDriver');
+Route.get("/create-driver", "ConducteursController.create").as("createDriver");
 
 // Route pour enregistrer un nouveau conducteur
-Route.post('/store-driver', 'ConducteursController.store').as('storeDriver');
+Route.post("/store-driver", "ConducteursController.store").as("storeDriver");
 
 // Route pour afficher la liste des conducteurs
-Route.get('/drivers', 'ConducteursController.index').as('listDrivers');
+Route.get("/drivers", "ConducteursController.index").as("listDrivers");
 
 // Route pour afficher le formulaire de modification d'un conducteur
-Route.get('/edit-driver/:id', 'ConducteursController.edit').as('editDriver');
+Route.get("/edit-driver/:id", "ConducteursController.edit").as("editDriver");
 
 // Route pour mettre à jour les informations d'un conducteur
-Route.post('/update-driver/:id', 'ConducteursController.update').as('updateDriver');
+Route.post("/update-driver/:id", "ConducteursController.update").as(
+  "updateDriver"
+);
 
 // Route pour supprimer un conducteur
-Route.get('/delete-driver/:id', 'ConducteursController.destroy').as('destroyDriver');
+Route.get("/delete-driver/:id", "ConducteursController.destroy").as(
+  "destroyDriver"
+);
 
+Route.get("/edit-document", "DocumentsController.index").as("doc.list");
 
-
-Route.get('/edit-document', 'DocumentsController.index').as('doc.list');
-
-Route.get('/documents/create', 'DocumentsController.create').as('doc.create');
-Route.post('/documents', 'DocumentsController.store').as('doc.store');
-Route.get('/documents/:id', 'DocumentsController.show').as('doc.show');
-Route.get('/documents/:id/edit', 'DocumentsController.edit').as('doc.edit');
-Route.post('/documents/:id', 'DocumentsController.update').as('doc.update');
-Route.get('/delete-documents/:id', 'DocumentsController.destroy').as('doc.destroy');
+Route.get("/documents/create", "DocumentsController.create").as("doc.create");
+Route.post("/documents", "DocumentsController.store").as("doc.store");
+Route.get("/documents/:id", "DocumentsController.show").as("doc.show");
+Route.get("/documents/:id/edit", "DocumentsController.edit").as("doc.edit");
+Route.post("/documents/:id", "DocumentsController.update").as("doc.update");
+Route.get("/delete-documents/:id", "DocumentsController.destroy").as(
+  "doc.destroy"
+);
 
 // Enregistrement d'achat
-Route.get('/achats/create', 'AchatsController.create').as('achat.create')
-Route.post('/achats', 'AchatsController.store').as('achat.store')
+Route.get("/achats/create", "AchatsController.create").as("achat.create");
+Route.post("/achats", "AchatsController.store").as("achat.store");
 
 // Mise à jour d'achat
-Route.get('/achats/:id/edit', 'AchatsController.edit').as('achat.edit')
-Route.get('/achats/:id', 'AchatsController.update').as('achat.update')
+Route.get("/achats/:id/edit", "AchatsController.edit").as("achat.edit");
+Route.get("/achats/:id", "AchatsController.update").as("achat.update");
 
 // Suppression d'achat
-Route.get('/delete-achats/:id', 'AchatsController.destroy').as('achat.destroy')
+Route.get("/delete-achats/:id", "AchatsController.destroy").as("achat.destroy");
 
 // Visualisation d'achat
-Route.get('/achats', 'AchatsController.index').as('achat.list')
+Route.get("/achats", "AchatsController.index").as("achat.list");
 // Route.get('/achats/:id', 'AchatsController.show')
 
-Route.get('/locations', 'LocationsController.index').as('locations.list')
-Route.get('/locations/create', 'LocationsController.create').as('locations.create')
-Route.post('/locations', 'LocationsController.store').as('locations.store')
-Route.get('/locations/:id/edit', 'LocationsController.edit').as('locations.edit')
-Route.get('/locations/:id', 'LocationsController.update').as('locations.update')
-Route.get('/delete-locations/:id', 'LocationsController.destroy').as('locations.destroy')
+Route.get("/locations", "LocationsController.index").as("locations.list");
+Route.get("/locations/create", "LocationsController.create").as(
+  "locations.create"
+);
+Route.post("/locations", "LocationsController.store").as("locations.store");
+Route.get("/locations/:id/edit", "LocationsController.edit").as(
+  "locations.edit"
+);
+Route.get("/locations/:id", "LocationsController.update").as(
+  "locations.update"
+);
+Route.get("/delete-locations/:id", "LocationsController.destroy").as(
+  "locations.destroy"
+);
 
 // payments
-Route.get('/payments', 'PaymentsController.index').as('payments.index')
-Route.get('/payments/create', 'PaymentsController.create').as('payments.create')
-Route.post('/payments', 'PaymentsController.store').as('payments.store')
+Route.get("/payments", "PaymentsController.index").as("payments.index");
+Route.get("/payments/create", "PaymentsController.create").as(
+  "payments.create"
+);
+Route.post("/payments", "PaymentsController.store").as("payments.store");
 
 //register
 // Route
-Route.get('/register', 'LoginController.showRegisterForm').as('register')
-Route.post('/register', 'LoginController.register').as('register.create') 
+Route.get("/register", "LoginController.showRegisterForm").as("register");
+Route.post("/register", "LoginController.register").as("register.create");
 
 // rapport
 // start/routes.js
 
-Route.get('/reports/cost', 'RapportsController.cost').as('rapport.achat')
+Route.get("/reports/cost", "RapportsController.cost").as("rapport.achat");
 
-Route.get('/reports', 'RapportsController.cost').as('rapport.usage')
-
+Route.get("/reports", "RapportsController.cost").as("rapport.usage");
